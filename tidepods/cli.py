75287ff8f5df32022ef87d5abf9114fce95445a9
vlro <vlro@dhigroup.com>
2018-03-05 14:58:34 +0100

    CLI changes

--- tidepods/cli.py
+++ tidepods/cli.py
@@ -1,4 +1,5 @@
 import click
+from datetime import datetime as dt
 
 @click.group()
 def cli():
@@ -6,9 +7,9 @@ def cli():
     pass
 
 @cli.command()
-@click.argument('shp', type=click.Path(dir_okay=False))
-@click.argument('template', type=click.Path(dir_okay=False))
-@click.argument('acq_date', type=click.INT)
+@click.option('-s','--shp', type=click.Path(dir_okay=False, required=True))
+@click.option('-t', '--template' type=click.Path(dir_okay=False), required=True)
+@click.option('-d', '--date', type=click.INT, required=True)
 @click.argument('pfs', type=click.Path(dir_okay=False))
 
 def pfs(**kwargs):
@@ -18,18 +19,24 @@ def pfs(**kwargs):
     
     Arguments:
     
-    shp : Path to AOI shapefile e.g. C:/tides/aoi.shp
-    
-    template : Path to template textfile e.g. C:/tides/template.txt
-    
-    acq_date : Image acquisiton date (YYYYMMDD)
-    
     pfs : Path to output pfs file e.g. C:/tides/outpfs.pfs
     
+    Options:
+    
+    -s/--shp : Path to AOI shapefile e.g. C:/tides/aoi.shp
+    
+    -t/--template : Path to template textfile e.g. C:/tides/template.txt
+    
+    -d/--date: Image acquisiton date (YYYYMMDD)
     Example use:
     
-    tidepods pfs C:/tides/aoi.shp C:/tides/template.txt 20171031 C:/tides/outpfs.pfs
+    tidepods pfs -s C:/tides/aoi.shp -t C:/tides/template.txt -d 20171031 C:/tides/outpfs.pfs
     """
     from . import write_pfs as wp
+    try:
+        return dt.strptime(s, "%Y-%m-%d")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise Exception(msg)
     wp.write_pfs(**kwargs)
     
