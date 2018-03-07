b8831b0f3732ccc4dd9eb511eba254b45a363040
vlro <vlro@dhigroup.com>
2018-03-07 09:58:41 +0100

    Style changes to comply with GRAS style guide

--- tidepods/write_pfs.py
+++ tidepods/write_pfs.py
@@ -1,86 +1,92 @@
-import fiona
-import numpy as np
-from shapely.geometry import shape, Point
-from fiona.transform import transform_geom
 from string import Template
 
+import fiona
+from fiona.transform import transform_geom
+import numpy as np
+from shapely.geometry import shape, Point
+
+
 def transform_shape(infile):
-    """Reprojects polygon to EPSG:4326 and return as shapely object
+    '''Reprojects polygon to EPSG:4326 and return as shapely object
 
     Parameters
     ----------
-    infile: str
-        path to polygon
+    infile : str
+            File path to polygon.
 
     Returns
     -------
-    shapely object : polygon
-    """
+    shp : shapely object
+
+    '''
     with fiona.open(infile) as c:
         infile_4326 = transform_geom(c.crs.get("init"), 'epsg:4326', c[0]['geometry'])
         shp = shape(infile_4326)
-    return (shp)
+    return shp
+
 
 def generate_pts(infile):
-    """Generates fixed distance points within a polygon
+    '''Generates fixed distance points within a polygon
 
     Parameters
     ----------
-    infile: str
-        path to polygon
+    infile : str
+            File path to polygon.
 
     Returns
     -------
-    list : list of shapely points for points within infile
-    """
+    plist : list
+            List of shapely points for points within infile
+
+    '''
     shp = transform_shape(infile)
     bounds = shp.bounds
     minx, miny, maxx, maxy = bounds
-    plist=[]
+    plist = []
     for x in np.arange(minx, maxx, 0.125):
         for y in np.arange(miny, maxy, 0.125):
-            p = (Point(x,y))
+            p = (Point(x, y))
             if p.within(shp):
                 plist.append(p)
-    return (plist)
+    return plist
+
 
 def write_pfs(shp, template, date, pfs):
-    """Creates pfs file for input to MIKE
+    '''Creates pfs file for input to MIKE
 
     Parameters
     ----------
-    shp: str
-        path to input shapefile
-    
-    template: str
-        path to template textfile
+    shp : str
+          File path to polygon.
 
-    date: str
-        image acquisiton date (YYYYMMDD)
-        
-    pfs: str
-        path to pfs file
+    template : str
+               File path to template textfile.
 
-    Returns
-    -------
-    pfs file: a .pfs file for input to MIKE
-    """
+    date : datetime
+           Image acquisiton date (YYYYMMDD).
+
+    pfs : str
+          File path to pfs file.
+
+    '''
     date_str = str(date)
     acq_year = date_str[:4]
     plist = generate_pts(shp)
     formatted_pts = []
     pid = 1
+    newline = '\n'
     for p in plist:
-        formatted_pts.append(r'[Point_' + str(pid) + ']\n' +
-                        r'description = ' + str(pid) + '\n' +
-                        r'y = ' + str(p.coords[0][0]) + '\n' +
-                        r'x = ' + str(p.coords[0][1]) + '\n' +
-                        r'EndSect // Point_' + str(pid) + '\n'
-            )
-        pid +=1
-    
+        point_text = ''.join(['[Point_', str(pid), ']', newline,
+                              'description = ', str(pid), newline,
+                              'y = ', str(p.coords[0][0]), newline,
+                              'x = ', str(p.coords[0][1]), newline,
+                              'EndSect // Point_', str(pid), newline])
+        formatted_pts.append(point_text)
+        pid += 1
+
     with open(template) as infile, open(pfs, 'w') as outfile:
-        d = {'REPLACE_YEAR': acq_year, 'REPLACE_OUTFILE_NAME': pfs, 'REPLACE_NUM_PTS': len(plist), 'REPLACE_POINTS':'\n'.join(formatted_pts)}
+        d = {'REPLACE_YEAR': acq_year, 'REPLACE_OUTFILE_NAME': pfs[:-4],
+             'REPLACE_NUM_PTS': len(plist), 'REPLACE_POINTS': '\n'.join(formatted_pts)}
         src = Template(infile.read())
         replacement = src.substitute(d)
         outfile.write(replacement)
