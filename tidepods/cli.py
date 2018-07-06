60ab0fb14b22ad5782c87a346916857ef964094e
Mike <29372465+vlro@users.noreply.github.com>
2018-07-06 14:28:01 +0200

    spelling and exmaple fixes

--- tidepods/cli.py
+++ tidepods/cli.py
@@ -42,7 +42,7 @@ def cli():
 @click.option('-d', '--date', type=DATEIN, required=True,
               help='Image acquisiton date (yyyymmdd) e.g. 20150131')
 @click.option('-o', '--outfile', type=click.Path(dir_okay=False), required=True,
-              help='Path to output points shapefile e.g. C:/tides/points.shp')
+              help='Path to output points shapefile e.g. C:/tides/pts.shp')
 @click.option('-p', '--pfs', type=click.Path(dir_okay=False), required=True,
               help='Path to output pfs file e.g. C:/tides/output.pfs')
 def pfs(**kwargs):
@@ -50,8 +50,8 @@ def pfs(**kwargs):
 
     Example use:
 
-    tidepods pfs -s C:/tides/aoi.shp -t C:/tides/template.txt -d 20170131
-    -o C:/tides/outpts.shp -p C:/tides/outpfs.pfs
+    tidepods pfs -i C:/tides/aoi.shp -t C:/tides/template.txt -d 20170131
+    -o C:/tides/pts.shp -p C:/tides/outpfs.pfs
     """
     from . import pfs_shp as ps
     ps.write_pfs(**kwargs)
@@ -68,7 +68,7 @@ def pfs(**kwargs):
 @click.option('-s', '--shapefile', type=click.Path(dir_okay=False, exists=True), required=True,
               help='Path to input pts shapefile created using tidepods pfs e.g. C:/tides/pts.shp')
 @click.option('-o', '--outfile', type=click.Path(dir_okay=False), required=True,
-              help='Path to output points shapefile containing the tidal values')
+              help='Path to output points shapefile containing the tidal values e.g. C:/tides/pts_tides.shp')
 @click.option('-p', '--sdkpath', type=click.Path(dir_okay=True, file_okay=False),
               default=r'C:\Program Files (x86)\DHI\2016\MIKE SDK\bin', show_default=True,
               help='Path to DHI SDK install directory')
@@ -78,7 +78,7 @@ def tides(date, timestamp, **kwargs):
     Example use:
 
     tidepods tides -p "C:/Program Files (x86)/DHI/2016/MIKE SDK/bin" -i C:/tides/aoi.dfs0
-    -d 20150131 -t 12:34 -s C:/tides/points.shp -o C:/tide/tidal_points.shp
+    -d 20150131 -t 12:34 -s C:/tides/pts.shp -o C:/tides/pts_tides.shp
     """
     kwargs.update(date=dt.combine(date, timestamp))
     from . import write_tides as wt
